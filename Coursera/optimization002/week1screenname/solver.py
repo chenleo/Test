f1eff00b5295d17c2f8a818a6ff8bc8013eef494
maverickmw <maverickcc@gmail.com>
2014-03-07 17:16:44 +0100

    add optiization week2

---
+++ Coursera/optimization002/week1screenname/solver.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+def solve_it():
+    # return a string that is the screen name you would like
+    return 'Leo Chen'
+
+if __name__ == '__main__':
+    print 'This script submits the screen name: ' + solve_it() + '\n'
