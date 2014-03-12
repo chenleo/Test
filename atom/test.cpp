24286477d40f8bd93bea4bb07daf446bc68d65da
maverickmw <maverickcc@gmail.com>
2014-03-12 11:55:26 +0100

    add atom

---
+++ atom/test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  cout << "Hello World!" << endl;
+  vector<int> test;
+
+
+  return 0;
+}
+
+class Cat {
+  Cat(int a, int b):a(a),b(b) {}
+private:
+public:
+}
